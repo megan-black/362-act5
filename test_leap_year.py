1c76bd11cec97e305420df2dba0454f3b2481693
megan black <blackme@oregonstate.edu>
2021-05-22 15:41:06 -0700

    Written leap year code and ran tests

--- test_leap_year.py
+++ test_leap_year.py
@@ -5,15 +5,29 @@
 import unittest
 import pytest
 
+# Function to get if leap year or not
+def leap(year):
+  year = int(year)
+  if year % 4 == 0 and year % 100 == 0 and year % 400 == 0:
+    return year + " is a leap year"
+  elif year % 4 == 0 and year % 100 != 0:
+    return str(year) + " is a leap year"
+  else:
+    return str(year) + " is not a leap year"
+
 class TestCase(unittest.TestCase):
-    def test_good(self):
-        self.assertEqual(leap(2020), "2020 is a leap year")
-        self.assertEqual(leap(1500), "2020 is not a leap year")
-        self.assertEqual(leap(1936), "1936 is a leap year")
-    def test_bad(self):
-        self.assertEqual(leap("abcd"), "Error")
-        self.assertEqual(leap(12345), "Error")
-        self.assertEqual(leap([2000, 2001]), "Error")
+  def test_good1(self):
+    self.assertEqual(leap(2020), "2020 is a leap year")
+  def test_good2(self): 
+    self.assertEqual(leap(1500), "1500 is not a leap year")
+  def test_good3(self):
+    self.assertEqual(leap(1936), "1936 is a leap year")
+  def test_bad1(self):
+    self.assertRaises(ValueError, leap, "abcd")
+  def test_bad2(self):
+    self.assertEqual(leap(12345), "Error")
+  def test_bad3(self):
+    self.assertRaises(TypeError, leap, [2000, 2001])
 
 if __name__ == "__main__":
     unittest.main(verbosity=2)
